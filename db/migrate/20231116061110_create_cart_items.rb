28e0608cd6d8ceb184af2f477929bba9481402a6
WakasugiSatoi <wakasugi.sa101@gmail.com>
2023-11-17 06:11:10 +0000

    Add column

--- db/migrate/20231116061110_create_cart_items.rb
+++ db/migrate/20231116061110_create_cart_items.rb
@@ -1,8 +1,12 @@
 class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
-
-      t.timestamps
+      
+      t.integer :item_id, null: false
+      t.integer :costomer_id, null: false
+      t.integer :amount,  null: false
+      
+      t.timestamps 
     end
   end
 end
